8717029ce5ec8b2b12cbfae555e4dea0534833b8
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2023-12-06 16:59:15 +0700

    Cut Discord RPC and GameSDK functionality to save more space

--- load/gamesdk.lua
+++ load/gamesdk.lua
@@ -1,5 +1,3 @@
-
-print("Loading Discord GameSDK...")
 DiscordGameSDK = {
 	loaded = false,
 	references = {},
@@ -12,23 +10,24 @@ DiscordGameSDK = {
 		small_text = ""
 	}
 }
-local now = 0
-local success, libDiscordGameSDK = pcall(require, "libs.discordGameSDK")
-if success then
-	DiscordGameSDK.loaded = true
-	DiscordGameSDK.app_id = 599778517789573120
+-- local now = 0
+-- local success, libDiscordGameSDK = pcall(require, "libs.discordGameSDK")
+-- if success then
+-- 	DiscordGameSDK.loaded = true
+-- 	DiscordGameSDK.app_id = 599778517789573120
 
-	DiscordGameSDK.references = libDiscordGameSDK.initialize(DiscordGameSDK.app_id)
-	DiscordGameSDK.presence.start_time = os.time(os.date("*t"))
-	DiscordGameSDK.references = libDiscordGameSDK.updatePresence(DiscordGameSDK.references, DiscordGameSDK.presence)
+-- 	DiscordGameSDK.references = libDiscordGameSDK.initialize(DiscordGameSDK.app_id)
+-- 	DiscordGameSDK.presence.start_time = os.time(os.date("*t"))
+-- 	DiscordGameSDK.references = libDiscordGameSDK.updatePresence(DiscordGameSDK.references, DiscordGameSDK.presence)
 
-	DiscordGameSDK.GameSDK = libDiscordGameSDK
-	print("Discord GameSDK successfully loaded")
-else
-	print("Discord GameSDK failed to load!")
-end
+-- 	DiscordGameSDK.GameSDK = libDiscordGameSDK
+-- 	print("Discord GameSDK successfully loaded")
+-- else
+-- 	print("Discord GameSDK failed to load!")
+-- end
 
 function DiscordGameSDK:update(newstuff)
-	for k, v in pairs(newstuff) do self.presence[k] = v end
-	if self.loaded then self.references = self.GameSDK.updatePresence(self.references, self.presence) end
+	print("DiscordGameSDK has been cut from Cambridge: Minified.")
+	-- for k, v in pairs(newstuff) do self.presence[k] = v end
+	-- if self.loaded then self.references = self.GameSDK.updatePresence(self.references, self.presence) end
 end
--- load/rpc.lua
+++ load/rpc.lua
@@ -1,59 +1,61 @@
-print("Loading discord RPC...")
+
 DiscordRPC = {
 	loaded = false
 }
-local now = 0
-local success, RPC = pcall(require, "libs.discordRPC")
-if success then
-	DiscordRPC.loaded = true
-	DiscordRPC.appId = "599778517789573120"
+-- local now = 0
+-- local success, RPC = pcall(require, "libs.discordRPC")
+-- if success then
+-- 	DiscordRPC.loaded = true
+-- 	DiscordRPC.appId = "599778517789573120"
 
-	function RPC.ready(userId, username, discriminator, avatar)
-		print(string.format("Discord: ready (%s, %s, %s, %s)", userId, username, discriminator, avatar))
-	end
+-- 	function RPC.ready(userId, username, discriminator, avatar)
+-- 		print(string.format("Discord: ready (%s, %s, %s, %s)", userId, username, discriminator, avatar))
+-- 	end
 
-	function RPC.disconnected(errorCode, message)
-		print(string.format("Discord: disconnected (%d: %s)", errorCode, message))
-	end
+-- 	function RPC.disconnected(errorCode, message)
+-- 		print(string.format("Discord: disconnected (%d: %s)", errorCode, message))
+-- 	end
 
-	function RPC.errored(errorCode, message)
-		print(string.format("Discord: error (%d: %s)", errorCode, message))
-	end
+-- 	function RPC.errored(errorCode, message)
+-- 		print(string.format("Discord: error (%d: %s)", errorCode, message))
+-- 	end
 
-	function RPC.joinGame(joinSecret)
-		print(string.format("Discord: join (%s)", joinSecret))
-	end
+-- 	function RPC.joinGame(joinSecret)
+-- 		print(string.format("Discord: join (%s)", joinSecret))
+-- 	end
 
-	function RPC.spectateGame(spectateSecret)
-		print(string.format("Discord: spectate (%s)", spectateSecret))
-	end
+-- 	function RPC.spectateGame(spectateSecret)
+-- 		print(string.format("Discord: spectate (%s)", spectateSecret))
+-- 	end
 
-	function RPC.joinRequest(userId, username, discriminator, avatar)
-		print(string.format("Discord: join request (%s, %s, %s, %s)", userId, username, discriminator, avatar))
-		RPC.respond(userId, "yes")
-	end
+-- 	function RPC.joinRequest(userId, username, discriminator, avatar)
+-- 		print(string.format("Discord: join request (%s, %s, %s, %s)", userId, username, discriminator, avatar))
+-- 		RPC.respond(userId, "yes")
+-- 	end
 
-	RPC.initialize(DiscordRPC.appId, true)
-	now = os.time(os.date("*t"))
+-- 	RPC.initialize(DiscordRPC.appId, true)
+-- 	now = os.time(os.date("*t"))
 	
-	DiscordRPC.RPC = RPC
-	print("DiscordRPC successfully loaded.")
-else
-	print("DiscordRPC failed to load!")
-	print(RPC)
-end
+-- 	DiscordRPC.RPC = RPC
+-- 	print("DiscordRPC successfully loaded.")
+-- else
+-- 	print("DiscordRPC failed to load!")
+-- 	print(RPC)
+-- end
 
-DiscordRPC.presence = {
-		startTimestamp = now,
-		details = "Loading game...",
-		state = "",
-		largeImageKey = "icon2",
-		largeImageText = "Arcade Stacker",
-		smallImageKey = "",
-		smallImageText = ""
-}
+-- DiscordRPC.presence = {
+-- 		startTimestamp = now,
+-- 		details = "Loading game...",
+-- 		state = "",
+-- 		largeImageKey = "icon2",
+-- 		largeImageText = "Arcade Stacker",
+-- 		smallImageKey = "",
+-- 		smallImageText = ""
+-- }
 
+-- This function is off.
 function DiscordRPC:update(newstuff)
-	for k, v in pairs(newstuff) do self.presence[k] = v end
-	if self.loaded then self.RPC.updatePresence(self.presence) end
+	print("Discord RPC functionality has been cut from Cambridge: Minified.")
+	-- for k, v in pairs(newstuff) do self.presence[k] = v end
+	-- if self.loaded then self.RPC.updatePresence(self.presence) end
 end
